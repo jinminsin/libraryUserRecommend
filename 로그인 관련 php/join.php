f74a5955f285a5eee36746a3a846831503583eeb
jinminsin <45217459+jinminsin@users.noreply.github.com>
2020-10-09 20:45:31 +0900

    함수 변경
    버전에 맞지 않은 구식 함수 변경

--- 로그인 관련 php/join.php
+++ 로그인 관련 php/join.php
@@ -3,15 +3,17 @@
     $conn = mysqli_connect("127.0.0.1","root","root","library");
     mysqli_set_charset($conn,"utf8");
      
-   $id = $_POST[u_id];
-   $pw = $_POST[u_pw]; 
+   $id = $_POST['u_id'];
+   $pw = $_POST['u_pw']; 
 
-$result = mysql_query("SELECT * FROM user where _id ='$id' and pw='$pw'");
-$num=mysql_num_rows($result);
+$query = "SELECT * FROM user where _id ='$id' and pw='$pw'";
+$result = mysqli_query($conn, $query);
+$num=mysqli_num_rows($result);
 if ($num > 0) {
 echo "-1";
 } else {
-$result = mysql_query("INSERT INTO USERS(_id, pw, authority) VALUES('$id', '$pw',0)");
+$query = "INSERT INTO USERS(_id, pw, authority) VALUES('$id', '$pw',0)";
+$result = mysqli_query($conn, $query);
 echo "1";
 }
 
--- 로그인 관련 php/login.php
+++ 로그인 관련 php/login.php
@@ -3,11 +3,12 @@
     $conn = mysqli_connect("127.0.0.1","root","root","library");
     mysqli_set_charset($conn,"utf8");
      
-   $id = $_POST[u_id];
-   $pw = $_POST[u_pw]; 
+   $id = $_POST['u_id'];
+   $pw = $_POST['u_pw']; 
 
-$result = mysql_query("SELECT * FROM user where _id ='$id' and pw='$pw'");
-$num=mysql_num_rows($result);
+$query = "SELECT * FROM user where _id ='$id' and pw='$pw'";
+$result = mysqli_query($conn, $query);
+$num=mysqli_num_rows($result);
 if ($num > 0) {
 echo "1";
 } else {
