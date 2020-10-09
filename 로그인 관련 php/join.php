361d551430667b64246e6f03a4c398b840264220
jinminsin <45217459+jinminsin@users.noreply.github.com>
2020-10-09 16:37:09 +0900

    로그인 관련 php
    login -> select를 통해 row값이 존재하는 지 확인 -> 1, -1 반환값
    join -> select를 통해 row값이 존재하는 지 확인 -> 존재한다면 -1 반환값, 존재하지 않는다면 insert 시행 1 반환값

---
+++ 로그인 관련 php/join.php
@@ -0,0 +1,19 @@
+<?php
+    header("Content-Type: text/html;charset=UTF-8");
+    $conn = mysqli_connect("127.0.0.1","root","root","library");
+    mysqli_set_charset($conn,"utf8");
+     
+   $id = $_POST[u_id];
+   $pw = $_POST[u_pw]; 
+
+$result = mysql_query("SELECT * FROM user where _id ='$id' and pw='$pw'");
+$num=mysql_num_rows($result);
+if ($num > 0) {
+echo "-1";
+} else {
+$result = mysql_query("INSERT INTO USERS(_id, pw, authority) VALUES('$id', '$pw',0)");
+echo "1";
+}
+
+    mysqli_close($conn);
+?>
